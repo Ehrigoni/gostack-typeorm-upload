d1e9a794b7134e5b58e50e4c7b35e7703ca99e08
Ehrigoni <ehrigoni@gmail.com>
2020-09-12 09:59:30 -0300

    a

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,22 +1,40 @@
+import { getCustomRepository, getRepository, In } from 'typeorm';
 import csvParse from 'csv-parse';
 import fs from 'fs';
+// import multer from 'multer';
+
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CategoriesRepository from '../repositories/CategoriesRepository';
+
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
   async execute(filePath: string): Promise<Transaction[]> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
     const contactsReadStream = fs.createReadStream(filePath);
 
-    const parsers = csvParse({
+    const parseStream = csvParse({
       from_line: 2,
+      ltrim: true,
+      rtrim: true,
     });
 
-    const parseCSV = contactsReadStream.pipe(parsers);
+    const parseCSV = contactsReadStream.pipe(parseStream);
 
-    const transactions = [{}];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const categories = [{}];
+    const transactions: CSVTransaction[] = [];
+    const categories: string[] = [];
 
-    parseCSV.on('data', async line => {
+    parseCSV.on('data', line => {
       const [title, type, value, category] = line.map((cell: string) =>
         cell.trim(),
       );
@@ -24,13 +42,27 @@ class ImportTransactionsService {
       if (!title || !type || !value) return;
 
       categories.push(category);
-
       transactions.push({ title, type, value, category });
-    });
 
+    });
     await new Promise(resolve => parseCSV.on('end', resolve));
 
-    return { categories, transactions };
+    const existentCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existentCategoriesTitles = existentCategories.map(
+    (category: Category) => category.title,
+    );
+
+    const addCategoryTitles = categories.filter(
+      category => !existentCategoriesTitles.includes(category).filter((value,index,self)),
+    );
+
+    console.log(existentCategoriesTitles);
+    console.log(transactions);
   }
 }
 export default ImportTransactionsService;
